1fab8becebdda45a736e332227b9e2b03536eb58
18839 <18839@ma-web.nl>
2015-11-28 01:58:49 +0100

    Update Unit.cs

--- Assets/Scripts/PathFinding/Unit.cs
+++ Assets/Scripts/PathFinding/Unit.cs
@@ -8,18 +8,15 @@ public class Unit : MonoBehaviour
     private Transform _target;
     [SerializeField]
     private float speed = 5;
-    //private bool reset = false;
     Vector3[] path;
     private int _targetIndex;
 
     public event System.Action noPath;
 
-    //private bool reset = false;
 
     void Start()
     {
         PathRequestManager.RequestPath(transform.position, _target.position, OnPathFound);
-        //StartCoroutine("DynamicPath"); //maybe for dynamic
     }
 
     public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
@@ -41,27 +38,7 @@ public class Unit : MonoBehaviour
             GameObject.Destroy(gameObject);
         }
     }
-    /*void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            StopCoroutine("DynamicPath");
-            StartCoroutine("DynamicPath");
-        }
-    }
-    IEnumerator DynamicPath()
-    /*IEnumerator DynamicPath()
-    {
-        //float RefreshTime = .5f;
-        while (true)
-        {
-            reset = true;
-            PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
-            yield break;
-            //yield return new WaitForSeconds(RefreshTime);
-        }
-    }*/
-    //maybe for dynamic
+   
 
  
     IEnumerator FollowPath()
@@ -70,24 +47,16 @@ public class Unit : MonoBehaviour
 
         while (true)
         {
-            /*if (reset)
-            {
-                reset = false;
-                targetIndex = 0;
-                currentWaypoint = path[targetIndex];
-                path = new Vector3[0];
-            }*/
-            //maybe for dynamic
+           
             if (transform.position == currentWaypoint)
             {
-                //StopCoroutine("DynamicPath");
-                //StartCoroutine("DynamicPath");
+               
                 _targetIndex++;
-                if (_targetIndex >= path.Length /*&& !reset*/)
+                if (_targetIndex >= path.Length )
                 {
                     _targetIndex = 0;
                     path = new Vector3[0];
-                    //yield break;
+                  
                 }
                 currentWaypoint = path[_targetIndex];
             }
